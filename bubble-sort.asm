78fb3aab255f1bf58fc9a8e0ea7e0df8b42ac580
Runitor <sergeyrunitor@gmail.com>
2020-07-15 20:57:33 +1200

    Added working program and files for debugging

--- bubble-sort.asm
+++ bubble-sort.asm
@@ -6,20 +6,47 @@
 ; using the Bubble Sort algorithm.
 
 section .data
-EXIT_SuCCESS	equ	0
+EXIT_SUCCESS	equ	0
 SYS_exit	equ	60
 
 TRUE		equ	255
 FALSE		equ	0
 
-bList		db	10, 5, 12, 6, 20
-		db	18, 3, 15, 7, 30
+bList		db	10, 5, 12, 6, 30
+		db	18, 3, 15, 7, 20
 length		db	10
 
 section .text
 global _start
 _start:
-	; code goes here
+	mov	cl, byte [length]
+	dec	cl					; i = length - 1
+
+sortingLoop:
+	mov	bl, FALSE				; reset bool
+	mov	sil, 0					; reset counter
+
+nestedLoop:
+	mov	r9b, byte [bList + rsi]			; r9b = bList[rsi]
+	mov	r10b, byte [bList + rsi + 1]		; r10b = bList[rsi+1]
+
+	cmp	r9b, r10b				; if r9b <= r10b
+	jbe	NotGreater				; don't swap positions
+	mov	byte [bList + rsi + 1], r9b		; else swap positions
+	mov	byte [bList + rsi], r10b
+	mov	bl, TRUE				
+
+NotGreater:
+	inc	sil
+	cmp	sil, cl					; if sil != cl
+	jne	nestedLoop				; loop again
+	
+	cmp	bl, FALSE				; if nothing was swapped
+	je	last					; terminate
+
+	loop sortingLoop
+	
+
 last:
 	mov	rax, SYS_exit
 	mov	rdi, EXIT_SUCCESS
